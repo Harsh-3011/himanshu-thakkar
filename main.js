d2911606d7f991394ee6952f2ec47a69cb7ad801
Harsh Sorathiya <113373860+Harsh-3011@users.noreply.github.com>
2024-08-22 19:43:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// var navLinks = document.getElementById("navLinks");
+
+// 	function showmenu(){
+// 		navLinks.style.right = "0";
+// 	}
+// 	function hidemenu(){
+// 		navLinks.style.right = "-200px"
+// 	}
+
 window.addEventListener('scroll', function(){
     var scroll = document.querySelector('.scrolltoTop');
     scroll.classList.toggle("active" , window.scrollY > 350)
